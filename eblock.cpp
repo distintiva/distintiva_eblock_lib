4419f14920ad56b9b8b848ecbc94c367224a04cb
Jose Carlos <enteropositivo@gmail.com>
2019-12-25 17:31:37 +0100

    New functions

--- eblock.cpp
+++ eblock.cpp
@@ -2,12 +2,65 @@
 #include "eblock.h"
 
 
-volatile int e_pin_states=0;
+volatile uint64_t e_pin_states=0;
+
+//- To store mode values 0-3
+volatile uint64_t e_mode64_0=0;
+volatile uint64_t e_mode64_1=0;
+
+
+typedef struct {
+        uint8_t id;
+        void (*event)(uint8_t id);
+} E_T_EVENT;
+
+E_T_EVENT* events_list = NULL;
+uint8_t events_count =0; 
+
+uint8_t e_event_exists( uint8_t event_id ){
+    for(uint8_t e =0; e< events_count; e++){
+        if( events_list[e].id == event_id ) return true;
+    }
+    return false;    
+} 
+
+void e_event_call( uint8_t event_id ){
+  
+    for(uint8_t e =0; e< events_count; e++){
+        if( events_list[e].id == event_id ){
+           
+               events_list[e].event(event_id);  
+               return;
+        } 
+    }
+    
+} 
+
+void e_event( uint8_t event_id,   void (*callback_fn)(uint8_t id) ) {
+  
+//Serial.println("ADD" + event);
+
+  if(e_event_exists(event_id)) return;
+
+  //- Add callback to the event list
+    if(events_list == NULL ){
+      events_list = ( E_T_EVENT* ) calloc( ++events_count , sizeof(E_T_EVENT));
+    }else{
+       events_list = ( E_T_EVENT* ) realloc( events_list , ++events_count*sizeof(E_T_EVENT)  );
+    }
+     
+    events_list[events_count-1].id = event_id;
+    events_list[events_count-1].event = callback_fn;
+}
 
 
 uint8_t e_get_mode(uint8_t pin)
 {
-  uint8_t bit = digitalPinToBitMask(pin);
+  
+    uint8_t mode = bit2Read64( e_mode64_0, e_mode64_1, pin );
+    return  mode;
+
+ /* uint8_t bit = digitalPinToBitMask(pin);
   uint8_t port = digitalPinToPort(pin);
 
 
@@ -15,8 +68,8 @@ uint8_t e_get_mode(uint8_t pin)
 
   
   volatile uint8_t *reg, *out;
-  reg = portModeRegister(port);
-  out = portOutputRegister(port);
+  reg =  portModeRegister(port);
+  out =  portOutputRegister(port);
 
   if (*reg & bit)
     return OUTPUT;
@@ -24,6 +77,9 @@ uint8_t e_get_mode(uint8_t pin)
     return INPUT_PULLUP;
   else
     return INPUT;
+ */
+  
+  
 }
 
 // set mode if not already setted
@@ -31,30 +87,40 @@ void e_set_mode(uint8_t pin, uint8_t mode)
 {
   if( e_get_mode(pin)!= mode ) {
     pinMode(pin, mode);
+
+    //- store mode for this pin
+    bit2Write64(e_mode64_0, e_mode64_1, mode, pin);
+
     //- if mode changes reset button state of that pin
-    bitClear(e_pin_states, pin);
+    bitClear64(e_pin_states, pin);
   }
 }
 
 uint8_t e_is_button(uint8_t pin){
-  e_set_mode(pin, INPUT);
+   uint8_t pressed_state=1;
+  if( e_get_mode(pin)==INPUT_PULLUP ) {
+    pressed_state=0;
+  }else{
+    e_set_mode(pin, INPUT);
+  }
 
-  return digitalRead(pin) == LOW;
+  return digitalRead(pin)==pressed_state /*== LOW*/;
 }
 
-uint8_t e_is_button_down(uint8_t pin){
+uint8_t e_is_button_up(uint8_t pin){
   
-  if( e_is_button(pin) && bitRead(e_pin_states, pin)==0 ) {
-    bitSet(e_pin_states, pin);
+  if( !e_is_button(pin) && bitRead(e_pin_states, pin)==1 ) {
+      bitClear64(e_pin_states, pin);
     return 1;
   }
   return 0;
 }
 
-uint8_t e_is_button_up(uint8_t pin){
+uint8_t e_is_button_down(uint8_t pin){
   
-  if( !e_is_button(pin) && bitRead(e_pin_states, pin) ) {
-    bitClear(e_pin_states, pin);
+  if( e_is_button(pin) && bitRead(e_pin_states, pin)==0 ) {
+     bitSet64(e_pin_states, pin);
+  
     return 1;
   }
   return 0;
@@ -79,6 +145,34 @@ void e_pwm(uint8_t pin, uint8_t value){
    analogWrite(pin, value);
 
 }
+
+uint8_t e_touching(uint8_t pin){
+   return e_get_analog(pin)>10;
+}
+
+uint8_t e_touch_down(uint8_t pin){
+  if(pin< A0) pin=A0+pin;
+
+  if( e_touching(pin) && bitRead(e_pin_states, pin)==0 ) {
+    bitSet(e_pin_states, pin);
+    return 1;
+  }
+
+  return false;
+}
+
+
+
+uint8_t e_touch_up(uint8_t pin){
+  if(pin< A0) pin=A0+pin;
+  
+  if( !e_touching(pin) && bitRead(e_pin_states, pin) ) {
+    bitClear(e_pin_states, pin);
+    return 1;
+  }
+  return 0;
+}
+
 
 
 
--- eblock.h
+++ eblock.h
@@ -4,6 +4,17 @@
 #define eblock_h
 
 
+//- Stores 1 bit[0-1] in a specific position of a uint64_t variable ( like arr[0-63]=[0-1] )
+#define bitSet64(value, bit)   ((value) |= (1ULL << bit ))
+#define bitClear64(value, bit) ((value) &= ~(sizeof(value)<5?1UL:1ULL) <<(bit))
+#define bitWrite64(value, bit, bitvalue) (bitvalue ? bitSet64(value, bit) : bitClear64(value, bit))
+#define bitRead64(value, bit)  (bitRead(value, bit))
+
+//- Stores 2 bits value[0-3] in a specific position of a 2 given uint64_t variables ( like arr[0-63]=[0-3] )
+#define bit2Write64( u64_t0, u64_t1, val, pos )  ({ bitWrite64(u64_t0, pos, bitRead(val,0)  ); bitWrite64(u64_t1, pos, bitRead(val,1)  ); })
+#define bit2Read64( u64_t0, u64_t1, pos )        ( bitRead64(u64_t1, pos) <<1 | bitRead64(u64_t0, pos) )
+
+
 // Get the current mode of a pin
 extern uint8_t 	e_get_mode(uint8_t pin);
 // Sets pinMode only if not already setted with passed mode
@@ -26,10 +37,20 @@ extern uint16_t e_get_analog(uint8_t pin);
 extern uint8_t 	e_get_analog_perc(uint8_t pin);
 // Sets PWM
 extern void 	e_pwm(uint8_t pin, uint8_t value);
+// True if touching an analog pin
+extern uint8_t  e_touching(uint8_t pin);
+// True if analog pin goes from normal to pressed
+extern uint8_t e_touch_down(uint8_t pin);
+// True if analog pin goes from pressed to normal
+extern uint8_t e_touch_up(uint8_t pin);
 
 
-
-
-
+//=== Events ===
+// Add a function pointer to the events stack  (ex.: e_event(23, my_func) )
+extern void e_event( uint8_t event,   void (*callback_fn)(uint8_t id) ) ;
+// Check if exixts an event with a passed id
+extern uint8_t e_event_exists( uint8_t event_id );
+// Fire event by its id
+extern void e_event_call( uint8_t event_id );
 
 #endif
